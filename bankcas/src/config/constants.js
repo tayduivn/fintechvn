0dc332edfb678763eb1624925299692f9910d028
Châu Minh Thiện <minhthien1305@gmail.com>
2018-09-14 11:42:09 +0700

    fix code url api

--- bankcas/src/config/constants.js
+++ bankcas/src/config/constants.js
@@ -1,4 +1,5 @@
 export const KEY_LANG_BANKCAS = 'langBankcas';
 export const KEY_SESSION      = 'BanKcasSession';
 export const API_KEY          = 'FgQUvwaKRiXATWCFmhzHTA5iVCyvDRr6T0ntjBdhjOtA1iceaB1qp3buZfyqeOYR';
+export const URL_LOGIN        = 'http://account.financal.vn/';
 export const URL_BASE         = 'http://bankcas-s1.fintechvietnam.com.vn';
--- insurFrontEnd/src/config/constants.js
+++ insurFrontEnd/src/config/constants.js
@@ -1,4 +1,6 @@
 export const KEY_LANG_BANKCAS = 'langInsure';
 export const KEY_SESSION      = 'InsureSession';
 export const API_KEY          = 'FgQUvwaKRiXATWCFmhzHTA5iVCyvDRr6T0ntjBdhjOtA1iceaB1qp3buZfyQQQQQ';
+
+export const URL_LOGIN        = 'http://account.financal.vn/';
 export const URL_BASE         = 'http://insur-s1.fintechvietnam.com.vn';
