85f39b056c6f8de8e62f6d8b22e6aad6a17c795f
Châu Minh Thiện <minhthien1305@gmail.com>
2018-10-18 17:21:20 +0700

    fix url login

--- FrontendBankcasNew/src/config/constants.js
+++ FrontendBankcasNew/src/config/constants.js
@@ -1,7 +1,7 @@
 export const KEY_LANG_BANKCAS = 'langBankcas';
 export const KEY_SESSION      = 'BanKcasSession';
 export const API_KEY          = 'FgQUvwaKRiXATWCFmhzHTA5iVCyvDRr6T0ntjBdhjOtA1iceaB1qp3buZfyqeOYR';
-export const URL_LOGIN        = 'http://account.financal.vn';
+export const URL_LOGIN        = 'http://account.financial.net.vn';
 
 export const URL_BASE         = 'http://bankcas-s1.fintechvietnam.com.vn';
 // export const URL_BASE         = 'http://localhost:5000';
--- FrontendInsurNew/src/config/constants.js
+++ FrontendInsurNew/src/config/constants.js
@@ -1,7 +1,7 @@
 export const KEY_LANG_BANKCAS = 'langInsure';
 export const KEY_SESSION      = 'InsureSession';
 export const API_KEY          = 'FgQUvwaKRiXATWCFmhzHTA5iVCyvDRr6T0ntjBdhjOtA1iceaB1qp3buZfyQQQQQ';
-export const URL_LOGIN        = 'http://account.financal.vn';
+export const URL_LOGIN        = 'http://account.financial.net.vn';
 
 export const CODE             = 'NH';
 // export const URL_BASE         = 'http://localhost:3600';
