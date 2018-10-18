70a550fce7fbc366ffc4223387f3dc3ad5e44855
Châu Minh Thiện <minhthien1305@gmail.com>
2018-10-18 16:50:35 +0700

    upload assets

--- Dashboard/src/config/constants.js
+++ Dashboard/src/config/constants.js
@@ -3,4 +3,4 @@ export const KEY_SESSION      = 'DashboardSession';
 
 export const API_KEY          = 'FgQUvwaKRiXATWCFmhzHTA5iVCyvDRr6T0ntjBdhjOtA112ascaAcasc';
 
-export const URL_BASE         = 'http://localhost:5100';
+export const URL_BASE         = 'http://dashboard.fintechvietnam.com.vn';
