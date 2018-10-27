2ed60c51370465965566a0bb99d129991b0b0b5e
Châu Minh Thiện <minhthien1305@gmail.com>
2018-10-27 11:44:00 +0700

    realtime frontend

--- FrontendInsurNew/src/config/constants.js
+++ FrontendInsurNew/src/config/constants.js
@@ -4,11 +4,11 @@ export const API_KEY          = 'FgQUvwaKRiXATWCFmhzHTA5iVCyvDRr6T0ntjBdhjOtA1ic
 export const URL_LOGIN        = 'http://account.financial.net.vn';
 
 export const CODE             = 'NH';
-export const URL_BASE         = 'http://localhost:3600';
-// export const URL_BASE         = 'http://insur-s1.fintechvietnam.com.vn';
+// export const URL_BASE         = 'http://localhost:3600';
+export const URL_BASE         = 'http://insur-s1.fintechvietnam.com.vn';
 
-export const URL_BACK_BANKCAS    = 'http://localhost:5000';
-// export const URL_BACK_BANKCAS    = 'http://insur-s1.fintechvietnam.com.vn';
+// export const URL_BACK_BANKCAS    = 'http://localhost:5000';
+export const URL_BACK_BANKCAS    = 'http://bankcas-s1.fintechvietnam.com.vn';
 
 export const MIN_YEAR         = 0;
 export const MAX_YEAR         = 50;
