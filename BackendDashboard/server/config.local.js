b5ef0550059902f539557384bdaa773501564127
Châu Minh Thiện <minhthien1305@gmail.com>
2018-12-26 19:34:43 +0700

    update backend dash

--- BackendDashboard/server/config.local.js
+++ BackendDashboard/server/config.local.js
@@ -2,7 +2,7 @@ var p = require('../package.json');
 var version = p.version.split('.').shift();
 module.exports = {
   restApiRoot: '/api' + (version > 0 ? '/v' + version : ''),
-  // host: process.env.HOST || 'localhost',
-  host: process.env.HOST || '192.168.1.2',
+  host: process.env.HOST || 'localhost',
+  // host: process.env.HOST || '192.168.1.2',
   port: process.env.PORT || 5100,
 };
