d1d1a43d5cd546c76b2d9325b0c146d6c972c738
Châu Minh Thiện <minhthien1305@gmail.com>
2019-01-02 10:00:08 +0700

    fix tam model crm

--- BackendDashboardNew/common/models/car-line.js
+++ BackendDashboardNew/common/models/car-line.js
@@ -5,13 +5,15 @@ var Fun             = require('./../../libs/functions');
 module.exports = function(Carline) {
 
   /* Validate Data */
-  Carline.beforeValidate = function(next, fields) {
+  Carline.beforeValidate = function(next, fields) { console.log(fields)
     let { name, carManufacturerId } = fields;
+    // console.log(carManufacturerId)
+    
     if (!!name) Carline.validatesLengthOf('name', {
       min: 3, max: 250,
       message: {min: 'Name is too short', max: 'Name is too long'}});
-    if (!!carManufacturerId)
-      Carline.validatesFormatOf('carManufacturerId', {with: /^[a-z\d]{24}$/g, message: 'CarManufacturer id invalid'});
+    // if (!!carManufacturerId)
+      // Carline.validatesFormatOf('carManufacturerId', {with: /^[a-z\d]{24}$/g, message: 'CarManufacturer id invalid'});
 
     next();
   };
--- BackendDashboardNew/common/models/car-model.js
+++ BackendDashboardNew/common/models/car-model.js
@@ -10,8 +10,8 @@ module.exports = function(Carmodel) {
     if (!!name) Carmodel.validatesLengthOf('name', {
       min: 3, max: 250,
       message: {min: 'Name is too short', max: 'Name is too long'}});
-    if (!!carLineId)
-      Carmodel.validatesFormatOf('carLineId', {with: /^[a-z\d]{24}$/g, message: 'CarLine id invalid'});
+    // if (!!carLineId)
+      // Carmodel.validatesFormatOf('carLineId', {with: /^[a-z\d]{24}$/g, message: 'CarLine id invalid'});
 
     next();
   };
