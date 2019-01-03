51a1ab162ad7876a715a7dd68ffcc87e4fc3c1d9
Châu Minh Thiện <minhthien1305@gmail.com>
2019-01-03 11:48:32 +0700

    fix  rule collection carmodel

--- BackendDashboardNew/common/models/car-model.js
+++ BackendDashboardNew/common/models/car-model.js
@@ -5,19 +5,18 @@ var Fun             = require('./../../libs/functions');
 module.exports = function(Carmodel) {
 
   /* Validate Data */
-  // Carmodel.beforeValidate = function(next, fields) {
-  //   let { name, carLineId } = fields;
-  //   if (!!name) Carmodel.validatesLengthOf('name', {
-  //     min: 3, max: 250,
-  //     message: {min: 'Name is too short', max: 'Name is too long'}});
-  //   // if (!!carLineId)
-  //   //   Carmodel.validatesFormatOf('carLineId', {with: /^[a-z\d]{24}$/g, message: 'CarLine id invalid'});
-
-  //   next();
-  // };
-
   Carmodel.validatesLengthOf('name', { min: 3, max: 250, message: {min: 'Name is too short', max: 'Name is too long'}})
-
+  Carmodel.validateAsync('carLabelId', hasCarLabelId, { message: 'CarLabelId not found models' });
+  
+  async function hasCarLabelId(err, next) { 
+    if (!this.carLabelId) return next();
+    var CarLabel = Carmodel.app.models.carLabel;
+    let test = await CarLabel.exists(this.carLabelId);
+    if(!test) err();
+    return next();
+  }
+  // Carmodel.validatesFormatOf('carLabelId', {with: /^[a-z\d]{24}$/g, message: 'CarLine id invalid'});
+  
   /* disableRemoteMethodByName */
   const enabledRemoteMethods = ['find', 'patchAttributes', 'create', 'deleteById'];
   Fun.disableAllMethodsBut(Carmodel, enabledRemoteMethods);
